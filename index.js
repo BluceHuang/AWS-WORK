8312569330a723d2a8789ef1cec1470668c3006d
hy <2818704605@qq.com>
2020-09-01 01:08:51 +0800

    ✨feat(csv data handle; dynamodb save handle): init commit 1. add csv file read feature from s3; 2. add dynamodb save feature

---
+++ index.js
@@ -0,0 +1,125 @@
+const AWS = require("aws-sdk");
+const parse = require("csv-parse/lib/sync");
+const md5 = require("md5-node");
+const stringify = require("json-stringify-safe");
+
+AWS.config.region = "ap-east-1";
+const s3 = new AWS.S3();
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const columns = ["latitude", "longitude", "address"];
+columns.sort();
+const LATITUDE = "latitude";
+const LONGITUDE = "longitude";
+
+const batchWriteSize = 20;
+const tableName = "t_address";
+
+/**
+ * check csv data
+ */
+exports.checkCsvData = (data) => {
+  const records = parse(data, {
+    columns: true,
+    skip_empty_lines: true,
+    delimiter: ",",
+  });
+
+  console.log(JSON.stringify(records));
+
+  // check data is empty or not
+  if (!Array.isArray(records) || records.length === 0) {
+    throw "csv data format error";
+  }
+
+  // check clumns
+  const dataColumns = Object.keys(records[0]);
+  if (columns.length != dataColumns.length) {
+    throw "csv data error, clumn length mismatch";
+  }
+  dataColumns.sort();
+  for (let i = 0; i < columns.length; i++) {
+    if (columns[i] !== dataColumns[i]) {
+      throw "csv data error, column name mismatch";
+    }
+  }
+  return records;
+};
+
+exports.getBatchWriteData = (records, offset, size) => {
+  const requestParams = {
+    RequestItems: {},
+  };
+  requestParams.RequestItems[tableName] = [];
+  for (let j = 0; j < size; j++) {
+    const id = md5(JSON.stringify(records[offset + j]));
+    records[offset + j][LATITUDE] = parseFloat(records[offset + j][LATITUDE]);
+    records[offset + j][LONGITUDE] = parseFloat(records[offset + j][LONGITUDE]);
+    requestParams.RequestItems[tableName].push({
+      PutRequest: { Item: Object.assign({ id }, records[offset + j]) },
+    });
+  }
+  return requestParams;
+};
+
+exports.handler = async (event) => {
+  const srcBucket = event.Records[0].s3.bucket.name;
+  const srcKey = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, "")
+  );
+
+  // Infer the csv type from the file suffix.
+  const typeMatch = srcKey.match(/\.([^.]*)$/);
+  if (!typeMatch) {
+    console.log("Could not determine the csv type.");
+    return;
+  }
+
+  // Check that the csv type is supported
+  const csvType = typeMatch[1].toLowerCase();
+  if (csvType !== "csv") {
+    console.log(`Unsupported type`);
+    return;
+  }
+
+  console.log(`receive this event ${JSON.stringify(event)}`);
+
+  try {
+    const csvData = await s3
+      .getObject({ Bucket: srcBucket, Key: srcKey })
+      .promise();
+    console.log(JSON.stringify(csvData));
+    const records = this.checkCsvData(csvData.Body);
+    const total = records.length;
+    const loopTotal = Math.ceil(total / batchWriteSize);
+
+    for (let i = 0; i < loopTotal; i++) {
+      const offset = i * batchWriteSize;
+      const handleSize = i < loopTotal - 1 ? batchWriteSize : total - offset;
+      const requestParams = this.getBatchWriteData(records, offset, handleSize);
+      console.log("write to dynamodb ...");
+      docClient.batchWrite;
+      const result = await docClient.batchWrite(requestParams).promise();
+      console.log(stringify(result));
+      if (
+        result.UnprocessedItems &&
+        result.UnprocessedItems[tableName] &&
+        result.UnprocessedItems[tableName].length > 0
+      ) {
+        console.error(
+          `${tableName} unprocess data ${JSON.stringify(
+            result.UnprocessedItems
+          )}`
+        );
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
+  return response;
+};
---
+++ test/batchwrite.test.js
@@ -0,0 +1,68 @@
+const { getBatchWriteData } = require("../index");
+const data = [
+  {
+    latitude: "-43.58299805",
+    longitude: "146.89373497",
+    address: "840 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58259635",
+    longitude: "146.89402117",
+    address: "833 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58169878",
+    longitude: "146.89824631",
+    address: "870 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58095637",
+    longitude: "146.88651178",
+    address: "810 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58079479",
+    longitude: "146.88701991",
+    address: "812 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58074011",
+    longitude: "146.88635117",
+    address: "808 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58056905",
+    longitude: "146.88637626",
+    address: "806 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58037106",
+    longitude: "146.88647572",
+    address: "804 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+  {
+    latitude: "-43.58010117",
+    longitude: "146.88671156",
+    address: "800 COCKLE CREEK RD, RECHERCHE TAS 7109",
+  },
+];
+const batchWriteSize = 9;
+
+describe("test batch write", () => {
+  test("batch write request data", () => {
+    const request = getBatchWriteData(data, 0, 2);
+    console.log(JSON.stringify(request));
+  });
+
+  test("loop get batch write request", () => {
+    const total = data.length;
+    const loopTotal = Math.ceil(total / batchWriteSize);
+
+    for (let i = 0; i < loopTotal; i++) {
+      const offset = i * batchWriteSize;
+      const handleSize = i < loopTotal - 1 ? batchWriteSize : total - offset;
+      const requestParams = getBatchWriteData(data, offset, handleSize);
+      console.log(JSON.stringify(requestParams));
+    }
+  });
+});
