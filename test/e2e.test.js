ac46a5f5b4a4f53cd691371cb08fc459551765c1
hy <2818704605@qq.com>
2020-09-25 22:47:25 +0800

    🧪test: add error integration test

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -1,6 +1,8 @@
 const { handler } = require("../src/index");
 const event = require("../event.json");
+const errEvent = require("../event-error.json");
 const apiEvent = require("../event-api.json");
+const { InvalidDataError } = require("../src/error");
 
 describe("csv data test", () => {
   test("accpet s3 event", async () => {
@@ -8,6 +10,13 @@ describe("csv data test", () => {
     const result = await handler(event);
     console.log(JSON.stringify(result));
     expect(result.statusCode).toBe(0);
+  });
+
+  test("accpet s3 error data", async () => {
+    jest.setTimeout(10000);
+    const result = await handler(errEvent);
+    console.log(JSON.stringify(result));
+    expect(result.statusCode).toBe(InvalidDataError);
   });
 
   test("accept api event", async () => {
